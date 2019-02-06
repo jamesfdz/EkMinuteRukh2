a60153c2fcb54d7239becfd405711cac11846872
jamesfdz <jfernandes942@gmail.com>
2019-02-07 03:06:06 +0530

    v2

--- app/src/main/java/com/codingwithjames/ekminuterukh/MainActivity.java
+++ app/src/main/java/com/codingwithjames/ekminuterukh/MainActivity.java
@@ -1,1 +1,22 @@
-package com.codingwithjames.ekminuterukh;import android.os.Bundle;import android.support.v4.view.ViewPager;import android.support.v7.app.AppCompatActivity;public class MainActivity extends AppCompatActivity {    private ViewPager mViewPager;    private Page_Adapter adapter;    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);        mViewPager = findViewById(R.id.viewPager);        adapter = new Page_Adapter(getSupportFragmentManager());        mViewPager.setAdapter(adapter);        mViewPager.setCurrentItem(0);    }}
+package com.codingwithjames.ekminuterukh;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private Page_Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager = findViewById(R.id.viewPager);
+        adapter = new Page_Adapter(getSupportFragmentManager());
+        mViewPager.setAdapter(adapter);
+        mViewPager.setCurrentItem(0);
+    }
+}
